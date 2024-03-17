bc8dcb4c780444286d61343bb5dd9033fa0cb744
M. G <mingjia.guan@gmail.com>
2024-03-17 14:39:59 -0400

    preprocessing functions added

--- recommendation_system/recommendation_system.py
+++ recommendation_system/recommendation_system.py
@@ -1,20 +1,40 @@
+#import libraries
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
+lem = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
 
 class recommendation_system:
     
     def __init__(self, raw_data):
         self.raw_data = raw_data
-        self.tokenized = None
-        self.na_stopwords = None
-        self.lemmatized = None
+        self.processed = None
 
-    def tokenize(self):
-        pass
+    def preprocess(self):
+        #tokenize words
+        tokenized = word_tokenize(self.raw_data)
+        tokenized = tokenized.lower()
+        print("Tokenization Complete")
 
-    def remove_stopwords(self):
-        pass
+        #remove stopwords
+        stopwords_removed = [w for w in word_tokens if w not in stop_words]
+        print("Stopwords Removed")
 
-    def lemmatize(self):
-        pass
+        #lemmatization
+        lemmatized = [lem.lemmatize(w) for w in stopwords_removed]
+        print("Lemmatization Complete.")
+
+        self.processed = lemmatized
+        print("Processing Complete")
+
+    def __repr__(self):
+        if not self.processed:
+            return f"Raw text of length {len(self.raw_data)}. Awaiting processing."
+        else:
+            return f"Raw text of length {len(self.raw_data)}, processed into a list of length {len(self.processed)}. Awaiting further action."
+
+
 
     
