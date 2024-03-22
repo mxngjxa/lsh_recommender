62e3177aafb92951c010c0842b8cd98cd6756832
M. G <mingjia.guan@gmail.com>
2024-03-22 00:42:54 -0400

    testing file added

--- testing.py
+++ testing.py
@@ -1,13 +1,19 @@
-from app.recommendation_system import recommendation_system
+import os
 import sklearn
 from sklearn.datasets import fetch_20newsgroups
+from app.recommendation_system import recommendation_system
 
-newsgroups_train = fetch_20newsgroups(subset='train')
-dir(newsgroups_train)['DESCR', 'data', 'filenames', 'target', 'target_names']
+
+current_directory = os.getcwd()
+desired_directory = f'{current_directory}/.venv/sklearn_data'
+
+newsgroups_train = fetch_20newsgroups(subset='train', data_home=desired_directory)
+attributes = dir(newsgroups_train)
 
 
 def main():
-    print(len(newsgroups_train.data))
+    targets = ['DESCR', 'data', 'filenames', 'target', 'target_names']
+    print(newsgroups_train)
 
 
 if __name__ == "__main__":
